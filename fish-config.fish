45634c1d4211aa5b0284cc914a68b9c4a3465973
Kixiron <kixiron.contact@gmail.com>
2019-02-28 22:18:59 -0600

    Added text colors and more colors

--- fish-config.fish
+++ fish-config.fish
@@ -13,6 +13,8 @@ set -g _pink FF79C6
 set -g _purple BD93F9
 set -g _cyan 8BE9FD
 set -g _light_blue 6272A4
+set -g _orange FFB86C
+set -g _green 50FA7B
 
 # Programming
 abbr --add --global py python3
@@ -25,21 +27,22 @@ abbr --add --global gco git checkout
 abbr --add --global gcom git commit
 
 # Startup Message
-set o (set_color $_dark_blue)
-set m (set_color $_purple)
-set i (set_color $_cyan)
-set fish_greeting '                 '$o'___                        Test
-  ___======____='$m'-'$i'-'$m'-='$o')                   Test
-/T            \_'$i'--='$m'=='$o')                  Test
-''['' \ '$m'('$i'O'$m')   '$o'\~    \_'$i'-='$m'='$o')             Test
- \      / )J'$m'~~    '$o'\\'$i'-='$o')             Test
-  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)              Test
-   \_____/JJJ'$m'~~'$i'~~    '$o'\\             Test
-   '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\            Test
-  (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_         Test      
-  '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__     Test        
-   '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\    Test     
-          '$o'\V)     \_) '$m'\\\\'$i'\\\\JJ\\'$m'J\)   Test
-                      '$o'/'$m'J'$i'\\'$m'J'$o'T\\'$m'JJJ'$o'J)  Test
-                      (J'$m'JJ'$o'| \UUU)  Test
-                       (UU)    Test'
+set outer (set_color $_dark_blue)
+set middle (set_color $_purple)
+set inner (set_color $_cyan)
+set text (set_color $_orange)
+set fish_greeting '                 '$outer'___                        '$text'Test
+  ___======____='$middle'-'$inner'-'$middle'-='$outer')                   '$text'Test
+/T            \_'$inner'--='$middle'=='$outer')                  '$text'Test
+''['' \ '$middle'('$inner'O'$middle')   '$outer'\~    \_'$inner'-='$middle'='$outer')             '$text'Test
+ \      / )J'$middle'~~    '$outer'\\'$inner'-='$outer')             '$text'Test
+  \\\\___/  )JJ'$middle'~'$inner'~~   '$outer'\)              '$text'Test
+   \_____/JJJ'$middle'~~'$inner'~~    '$outer'\\             '$text'Test
+   '$middle'/ '$outer'\  '$inner', \\'$outer'J'$middle'~~~'$inner'~~     '$middle'\\            '$text'Test
+  (-'$inner'\)'$outer'\='$middle'|'$inner'\\\\\\'$middle'~~'$inner'~~       '$middle'L_'$inner'_         '$text'Test      
+  '$middle'('$outer'\\'$middle'\\)  ('$inner'\\'$middle'\\\)'$outer'_           '$inner'\=='$middle'__     '$text'Test        
+   '$outer'\V    '$middle'\\\\'$outer'\) =='$middle'=_____   '$inner'\\\\\\\\'$middle'\\\\    '$text'Test     
+          '$outer'\V)     \_) '$middle'\\\\'$inner'\\\\JJ\\'$middle'J\)   '$text'Test
+                      '$outer'/'$middle'J'$inner'\\'$middle'J'$outer'T\\'$middle'JJJ'$outer'J)  '$text'Test
+                      (J'$middle'JJ'$outer'| \UUU)  '$text'Test
+                       (UU)    '$text'Test'
