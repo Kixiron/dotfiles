ff00bd8a3f46eff708223d5b16887d13416a0f9a
Kixiron <kixiron.contact@gmail.com>
2019-03-02 10:25:47 -0600

    Worked on stats

--- fish/fish-config.fish
+++ fish/fish-config.fish
@@ -33,18 +33,22 @@ function fish_greeting
     set m (set_color $color_cyan)
     set i (set_color $color_purple)
     set t (set_color $color_pink)
-    set shell_prefix "/usr/bin/"
-    set x_dimension (xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-    set y_dimension (xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+
+    set os_suffix "\n \l "
+    set os (head -n1 /etc/issue | sed -e "s/^$os_suffix//") (uname -m)
+    set resolution (xrandr --current | grep '*' | uniq | awk '{ print $1 }' | cut -d 'x' -f1) "x" (xrandr --current | grep '*' | uniq | awk '{ print $1 }' | cut -d 'x' -f2)
+    set uptime (uptime | awk -F'( |,|:)+' '{ print $6, $7",", $8, "hours,", $9 ,"minutes." }')
+    set cpu (awk -F':' '/^model name/ { print $2 }' /proc/cpuinfo)
+    set total_mem (free | awk '/^Mem:/ { print int($2 / 1000000) }')
 
     echo '                 '$o'___
       ___======____='$m'-'$i'-'$m'-='$o')               '$t'User: $user'$o'
     /T            \_'$i'--='$m'=='$o')              '$t'Host: $host'$o'
-    '$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')              '$t'OS: (uname) (uname -m)'$o'
-     \      / )J'$m'~~    '$o'\\'$i'-='$o')              '$t'Uptime: (uptime | awk -F'( |,|:)+' '{print $6,$7",",$8,"hours,",$9,"minutes."}')'$o'
-      \\\\___/  )JJ'$m'~'$i'~~   '$o'\)               '$t'Test'$o'
-       \_____/JJJ'$m'~~'$i'~~    '$o'\\              '$t'Resolution: ' $x_dimension 'x' $y_dimension '$o'
-       '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\             '$t'Test'$o'
+    '$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')              '$t'OS: $os'$o'
+     \      / )J'$m'~~    '$o'\\'$i'-='$o')              '$t'Uptime: $uptime'$o'
+      \\\\___/  )JJ'$m'~'$i'~~   '$o'\)               '$t'Memory: $total_mem'$o'
+       \_____/JJJ'$m'~~'$i'~~    '$o'\\              '$t'Resolution: $resolution''$o'
+       '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\             '$t'CPU: $cpu'$o'
       (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_          '$t'Test'$o'
       '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__      '$t'Test'$o'
        '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\     '$t'Test'$o'
