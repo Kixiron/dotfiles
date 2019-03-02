d5e01478e7e283d35b2728a153ff72d69a6f1cc5
Kixiron <kixiron.contact@gmail.com>
2019-03-02 10:35:07 -0600

    Variables

--- fish/fish-config.fish
+++ fish/fish-config.fish
@@ -42,13 +42,13 @@ function fish_greeting
     set total_mem (free | awk '/^Mem:/ { print int($2 / 1000000) }')
 
     echo '                 '$o'___
-      ___======____='$m'-'$i'-'$m'-='$o')               '$t'User: $user'$o'
-    /T            \_'$i'--='$m'=='$o')              '$t'Host: $host'$o'
-    '$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')              '$t'OS: $os'$o'
-     \      / )J'$m'~~    '$o'\\'$i'-='$o')              '$t'Uptime: $uptime'$o'
-      \\\\___/  )JJ'$m'~'$i'~~   '$o'\)               '$t'Memory: $total_mem'$o'
-       \_____/JJJ'$m'~~'$i'~~    '$o'\\              '$t'Resolution: $resolution'$o'
-       '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\             '$t'CPU: $cpu'$o'
+      ___======____='$m'-'$i'-'$m'-='$o')               '$t'User: '$user $o'
+    /T            \_'$i'--='$m'=='$o')              '$t'Host: '$host $o'
+    '$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')              '$t'OS: '$os $o'
+     \      / )J'$m'~~    '$o'\\'$i'-='$o')              '$t'Uptime: '$uptime $o'
+      \\\\___/  )JJ'$m'~'$i'~~   '$o'\)               '$t'Memory: '$total_mem $o'
+       \_____/JJJ'$m'~~'$i'~~    '$o'\\              '$t'Resolution: '$resolution $o'
+       '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\             '$t'CPU: '$cpu $o'
       (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_          '$t'Test'$o'
       '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__      '$t'Test'$o'
        '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\     '$t'Test'$o'
