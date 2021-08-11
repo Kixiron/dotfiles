b02fc33865aaec0dbfb0c94461cc8ebbda963ff5
Chase Wilson <contact@chasewilson.dev>
2021-08-11 13:46:05 -0700

    Update Setup.ps1

--- powershell/Setup.ps1
+++ powershell/Setup.ps1
@@ -3,25 +3,25 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 Install-Module yarn-completion -Scope CurrentUser
 
 Add-WingetPackages @(
-    Mozilla.Firefox,
-    Git.Git,
-    Microsoft.VisualStudioCode,
-    Discord.Discord,
-    Valve.Steam,
-    OpenJS.Nodejs,
-    7zip.7zip,
-    Spotify.Spotify,
-    Python.Python,
-    GnuWin32.Make,
-    Kitware.CMake,
-    Microsoft.PowerToys,
-    Microsoft.PowerShell,
-    Microsoft.WindowsTerminalPreview,
-    LLVM.LLVM,
-    Files-UWP.FilesUWP,
-    AntibodySoftware.WizTree,
-    Yarn.Yarn,
-    OpenJS.NodeJS,
+    "Mozilla.Firefox",
+    "Git.Git",
+    "Microsoft.VisualStudioCode",
+    "Discord.Discord",
+    "Valve.Steam",
+    "OpenJS.Nodejs",
+    "7zip.7zip",
+    "Spotify.Spotify",
+    "Python.Python",
+    "GnuWin32.Make",
+    "Kitware.CMake",
+    "Microsoft.PowerToys",
+    "Microsoft.PowerShell",
+    "Microsoft.WindowsTerminalPreview",
+    "LLVM.LLVM",
+    "Files-UWP.FilesUWP",
+    "AntibodySoftware.WizTree",
+    "Yarn.Yarn",
+    "OpenJS.NodeJS"
 )
 
 choco install fzf --yes
@@ -29,7 +29,7 @@ choco install fzf --yes
 function Add-WingetPackages {
     param ([string[]] $Packages)
     
-    for ($Package In $Packages) {
+    foreach ($Package in $Packages) {
         winget install -e --id $Package
     }
 }
