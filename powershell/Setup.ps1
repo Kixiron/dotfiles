43dac522dbf4125b188e55b206529cf65870bec6
Chase Wilson <contact@chasewilson.dev>
2021-08-26 14:09:14 -0500

    Update Setup.ps1

--- powershell/Setup.ps1
+++ powershell/Setup.ps1
@@ -1,6 +1,19 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 Install-Module yarn-completion -Scope CurrentUser
+
+function Add-WingetPackages {
+    param ([string[]] $Packages)
+    
+    foreach ($Package in $Packages) {
+        winget install -e --id $Package --log
+    }
+}
 
 Add-WingetPackages @(
     "Mozilla.Firefox",
@@ -21,15 +34,22 @@ Add-WingetPackages @(
     "Files-UWP.FilesUWP",
     "AntibodySoftware.WizTree",
     "Yarn.Yarn",
-    "OpenJS.NodeJS"
+    "OpenJS.NodeJS",
+    "stack.stack",
+    "Microsoft.VisualStudio.2019.Community",
+    "Rustlang.rust-msvc"
 )
 
 choco install fzf --yes
 
-function Add-WingetPackages {
-    param ([string[]] $Packages)
-    
-    foreach ($Package in $Packages) {
-        winget install -e --id $Package
-    }
-}
+# Update rust
+rustup toolchain add nightly --force
+rustup update
+
+cargo install lsd --force
+cargo install cargo-make --force
+cargo install ripgrep --force
+cargo install tokei --force
+
+# Additional things to install:
+# https://github.com/DynamoRIO/dynamorio/releases/latest
